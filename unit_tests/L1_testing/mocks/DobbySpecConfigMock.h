1baed1cae08fa22c9cf9bebf6da4416ccf30007e
sethulakshmisk <ssk@synamedia.com>
2023-10-03 14:10:21 +0530

    Update DobbySpecConfigMock.h

--- unit_tests/L1_testing/mocks/DobbySpecConfigMock.h
+++ unit_tests/L1_testing/mocks/DobbySpecConfigMock.h
@@ -12,9 +12,9 @@ public:
 
     // Declare the mock method using the MOCK_METHOD macro
     MOCK_METHOD((const std::map<std::string, Json::Value>&), rdkPlugins, (), (const));
-    MOCK_METHOD(const std::string ,spec, (), (const,override));
+    MOCK_METHOD((const std::string) ,spec, (), (const));
     MOCK_METHOD(bool ,isValid, (), (const));
     MOCK_METHOD(std::shared_ptr<rt_dobby_schema> ,config, (), (const));
     MOCK_METHOD(bool ,restartOnCrash, (), (const));
-    MOCK_METHOD((const std::map<std::string, Json::Value>&), legacyPlugins, (), (const,override));
+    MOCK_METHOD((const std::map<std::string, Json::Value>&), legacyPlugins, (), (const));
 };
