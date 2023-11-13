f5a80bce0beadd730719a0a7739af538ccb65351
Siva Thandayuthapani <sithanda@synamedia.com>
2023-11-13 17:06:57 +0530

    adding test to invoke callback

--- unit_tests/L1_testing/tests/DobbyTest/DaemonDobbyTests.cpp
+++ unit_tests/L1_testing/tests/DobbyTest/DaemonDobbyTests.cpp
@@ -2584,9 +2584,16 @@ TEST_F(DaemonDobbyTest, startFromBundleSuccess_argSize_3_postWorkSuccess)
     EXPECT_CALL(*p_containerIdMock, isValid())
         .WillOnce(::testing::Return(true));
 
-    /* Simulates a successful start, returning a container descriptor ,which is a unique number that identifies the container */
-    EXPECT_CALL(*p_dobbyManagerMock, startContainerFromBundle(::testing::_,::testing::_,::testing::_,::testing::_,::testing::_,::testing::_))
-        .WillOnce(::testing::Return(12));
+#if 0
+    ON_CALL(*p_dobbyManagerMock, startContainerFromBundle(::testing::_,::testing::_,::testing::_,::testing::_,::testing::_,::testing::_))
+       .WillByDefault(::testing::Invoke(
+                          [&](const ContainerId &,
+                                               const std::string &bundlePath,
+                                               const std::list<int> &files,
+                                               const std::string &command,
+                                               const std::string &displaySocket,
+                                               const std::vector<std::string>& envVars){dobby_test->onContainerStarted(1,id)}));
+#endif
 
     EXPECT_CALL(*p_workQueueMock, postWork(::testing::_))
         .Times(1)
