cecaaf2832ffb659e843618c3ab8271a00af3ca3
Siva Thandayuthapani <sithanda@synamedia.com>
2023-11-16 08:43:45 +0530

    adding test to invoke callback

--- unit_tests/L1_testing/tests/DobbyTest/DaemonDobbyTests.cpp
+++ unit_tests/L1_testing/tests/DobbyTest/DaemonDobbyTests.cpp
@@ -2585,17 +2585,6 @@ TEST_F(DaemonDobbyTest, startFromBundleSuccess_argSize_3_postWorkSuccess)
         .WillOnce(::testing::Return(true));
 
 
-#if 1
-    ON_CALL(*p_dobbyManagerMock, startContainerFromBundle(::testing::_,::testing::_,::testing::_,::testing::_,::testing::_,::testing::_))
-       .WillByDefault(::testing::Invoke(
-                          [&](const ContainerId &id,
-                                               const std::string &bundlePath,
-                                               const std::list<int> &files,
-                                               const std::string &command,
-                                               const std::string &displaySocket,
-                                               const std::vector<std::string>& envVars){dobby_test->onContainerStarted(12,id);}));
-#endif
-
     /* Simulates a successful start, returning a container descriptor ,which is a unique number that identifies the container */
     EXPECT_CALL(*p_dobbyManagerMock, startContainerFromBundle(::testing::_,::testing::_,::testing::_,::testing::_,::testing::_,::testing::_))
         .WillOnce();
