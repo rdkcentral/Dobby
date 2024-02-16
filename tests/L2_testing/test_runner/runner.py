5e56fcfd3856a7f83a4d38bf45bd924fb5b40790
kgnanagurusamy <kgnanagurusamy@synamedia.com>
2024-02-16 16:25:43 +0530

    [RDK-46381] Fix the networking and command_line_containers related issues

--- tests/L2_testing/test_runner/runner.py
+++ tests/L2_testing/test_runner/runner.py
@@ -49,9 +49,8 @@ def run_all_tests():
             test_utils.print_unsupported_platform(test.__name__, test_utils.selected_platform)
         # Skipped these tests because they are all failing in the workflow and will be enabled once they are resolved
         elif (test_utils.selected_platform == test_utils.Platforms.github_workflow_vm) and \
-            ((test.__name__ == "gui_containers") or (test.__name__ == "command_line_containers") or
-            (test.__name__ == "start_from_bundle") or (test.__name__ == "thunder_plugin") or
-            (test.__name__ == "network_tests")):
+            ((test.__name__ == "gui_containers") or (test.__name__ == "network_tests") or
+            (test.__name__ == "start_from_bundle") or (test.__name__ == "thunder_plugin")):
             test_utils.print_unsupported_platform(test.__name__, test_utils.selected_platform)
         else:
             success, total = test.execute_test()
