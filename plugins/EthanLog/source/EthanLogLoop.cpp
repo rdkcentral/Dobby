2387e080c2dc2fee29c94e2be70668dea9c521ab
Ben Gray <ben.gray@sky.uk>
2025-08-18 16:20:23 +0100

    ENTDAI-1652: ethanlog improvements
    Changes:
     - increased the pipe size to 128kb (from default 64kb)
     - added an env var check to redirect ethanlog to the console (stderr / stdout)
     - added new vprintf API that returns the number of bytes written to the
       pipe or an error.

--- plugins/EthanLog/source/EthanLogLoop.cpp
+++ plugins/EthanLog/source/EthanLogLoop.cpp
@@ -121,6 +121,14 @@ int EthanLogLoop::addClient(const ContainerId& id, const std::string &tag,
         return -1;
     }
 
+    // attempt to increase the pipe size to 128kB, this is double the default
+    // of 64kB
+    const int loggingPipeSize = 128 * 1024;
+    if (fcntl(fds[1], F_SETPIPE_SZ, loggingPipeSize) != 0)
+    {
+        AI_LOG_SYS_WARN(errno, "failed to set pipe size for logging pipe");
+    }
+
     AI_LOG_DEBUG("create logging pipe : read=%d : write=%d", fds[0], fds[1]);
 
     // take the lock
