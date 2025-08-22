28c3eb7319ce99796c2c3c26222fcd16030d8987
ssitar583 <soundaryaa_sitaram@comcast.com>
2025-08-22 14:41:21 +0530

    Update test_utils.py

--- tests/L2_testing/test_runner/test_utils.py
+++ tests/L2_testing/test_runner/test_utils.py
@@ -358,7 +358,7 @@ def launch_container(container_id, spec_path):
     print_log("Launching container %s with spec %s" % (container_id, spec_path), Severity.debug)
 
     # Use DobbyTool to launch container
-    process = run_command_line(["DobbyTool", "start", container_id,spec_path])
+    process = run_command_line(["DobbyTool","-v", "start", container_id,spec_path])
     output = process.stdout
     print("STDOUT:", process.stdout)
     print("STDERR:", process.stderr)
